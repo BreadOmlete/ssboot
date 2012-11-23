6b1ae4aab558cdc719350de441727ffd14450b2e
Krzysztof Dziuba <krzysztof.dziuba@sonymobile.com>
2012-11-23 13:18:33 +0100

    Add board specific variable to makefile.

--- board/stm32f100c4t6/board.mk
+++ board/stm32f100c4t6/board.mk
@@ -2,3 +2,8 @@
 # Board specific Makefile for SSBoot
 # Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
 ########################################################################
+
+LINKER_SCRIPT=linker.ld
+STARTUP_FILE=startup.s
+STM32F10X_LD_VL=1
+SYSTEM_INIT_OBJ=system_stm32f10x.o
