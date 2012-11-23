81fd1b8aeaf0f73f79ab7e4159ad5690170fda72
Krzysztof Dziuba <krzysztof.dziuba@sonymobile.com>
2012-11-23 10:42:34 +0100

    Add board specific Makefile

---
+++ board/stm32f100c4t6/board.mk
@@ -0,0 +1,4 @@
+########################################################################
+# Board specific Makefile for SSBoot
+# Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
+########################################################################
