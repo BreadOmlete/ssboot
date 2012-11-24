09b692e2395f9609805664d399fe67b37c4a5077
Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
2012-11-24 15:43:49 +0100

    Add/modify headers

--- board/stm32f100c4t6/board.h
+++ board/stm32f100c4t6/board.h
@@ -1,9 +1,10 @@
 /*
-	SSBoot - STM32 SPI Bootloader for RPI robot
-
-	Include board specific headers
-	
-	Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
+ * SSBoot - STM32 SPI Bootloader for RPI robot
+ *
+ * Include board specific headers
+ *
+ * Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
+ * License: Simplified BSD License (see README.md)
 */
 
 #include "stm32f10x.h"
--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/*
+ * SSBoot - STM32 SPI Bootloader for RPI robot
+ *
+ * Main project file
+ *
+ * Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
+ * License: Simplified BSD License (see README.md)
+*/
+
 #include "board.h"
 
 int main()
