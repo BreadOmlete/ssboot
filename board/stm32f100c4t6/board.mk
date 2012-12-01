57eabc8b0ec0ff7ff093a0f59a11796da08b0b93
Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
2012-12-01 23:38:28 +0100

    Fix executing program from SRAM

--- board/stm32f100c4t6/board.mk
+++ board/stm32f100c4t6/board.mk
@@ -4,5 +4,5 @@
 ########################################################################
 
 LINKER_SCRIPT = linker.ld
-COPTS = -DSTM32F10X_LD_VL -mthumb -mthumb-interwork -mcpu=cortex-m3
+COPTS = -DSTM32F10X_LD_VL -mthumb -mno-thumb-interwork -mcpu=cortex-m3
 BOARD_OBJECTS = system_stm32f10x.o startup.o
