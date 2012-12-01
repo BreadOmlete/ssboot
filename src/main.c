a2532ac52078f1255049cc373830dec4465ce112
Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
2012-12-02 00:28:58 +0100

    SRAM execution fix

--- src/main.c
+++ src/main.c
@@ -9,13 +9,8 @@
 
 #include "board.h"
 
-unsigned char data_buffer[1024];
-
-
 int main()
 {
-	volatile int i = 0;
-
 	// AHB, APB1, APB2 clock not divided
 	RCC->CFGR &= ~(RCC_CFGR_HPRE | RCC_CFGR_PPRE1 | RCC_CFGR_PPRE2);
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -26,10 +21,7 @@ int main()
 
 	GPIOC->BSRR |= GPIO_BSRR_BS14;
 
-	while(1)
-	{
-		i++;
-	}
+	while(1);
 
 	return 0;
 }
