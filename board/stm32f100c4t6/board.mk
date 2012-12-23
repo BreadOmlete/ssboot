5fcc8ef6d8c4c2d97f30510ef0d3823a1861448f
Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
2012-12-23 23:41:48 +0100

    Support debugging code from SRAM
    Add linker script that puts everything into SRAM, so debugging
    using OpenOCD should be easier.

--- board/stm32f100c4t6/board.mk
+++ board/stm32f100c4t6/board.mk
@@ -3,6 +3,6 @@
 # Author: Krzysztof Dziuba <krzysztof.dziuba@gmail.com>
 ########################################################################
 
-LINKER_SCRIPT = linker.ld
+LINKER_SCRIPT = linker_ram.ld
 COPTS = -DSTM32F10X_LD_VL -mthumb -mthumb-interwork -mcpu=cortex-m3
 BOARD_OBJECTS = system_stm32f10x.o startup.o board.o
